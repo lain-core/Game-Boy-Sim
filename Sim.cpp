b926436cad1ad3b4721f47cc81c20a360ecd6bbe
storyjss <lefoustory@gmail.com>
2020-08-25 16:44:33 -0400

    print statement changed for make testing

--- Sim.cpp
+++ Sim.cpp
@@ -3,11 +3,10 @@
 #include <stdio.h>
 #include <cstdint>
 #include "Sim.h"
-#include "Y86.h"
 #include "Memory.h"
 
 using namespace std;
 
 int main(){
-	printf("I don't know how to use c++ smile\n");
+	printf("Sim is working.\n");
 }
