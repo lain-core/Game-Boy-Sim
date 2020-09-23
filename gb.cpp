5f4211f4ef85878c3fdd381df4ae9edba6a6640d
Hunter King <hking0036@gmail.com>
2020-09-23 14:30:52 -0400

    fixed width on trace()

--- gb.cpp
+++ gb.cpp
@@ -31,8 +31,8 @@ int main(){
 void gb::trace(){
 	printf("------------------------------------------------\n");
 	printf("The contents of the 8- and 16-bit registers are:\n");
-	printf("A: 0x%x F: 0x%x\nB: 0x%x C: 0x%x\nD: 0x%x E: 0x%x\nH: 0x%x L: 0x%x\n", getRegisters().getReg8(A), getRegisters().getReg8(F), getRegisters().getReg8(B), getRegisters().getReg8(C) , getRegisters().getReg8(D), getRegisters().getReg8(E), getRegisters().getReg8(H), getRegisters().getReg8(L));
-	printf("AF: 0x%x, BC: 0x%x, DE: 0x%x, HL: 0x%x, SP: 0x%x\n", getRegisters().getReg16(AF), getRegisters().getReg16(BC), getRegisters().getReg16(DE), getRegisters().getReg16(HL), getRegisters().getReg16(SP));
-	printf("Last used Memory location: 0x%x, which contains:\n", getMemory().trace());
+	printf("A: 0x%02x F: 0x%02x\nB: 0x%02x C: 0x%02x\nD: 0x%02x E: 0x%02x\nH: 0x%02x L: 0x%02x\n", getRegisters().getReg8(A), getRegisters().getReg8(F), getRegisters().getReg8(B), getRegisters().getReg8(C) , getRegisters().getReg8(D), getRegisters().getReg8(E), getRegisters().getReg8(H), getRegisters().getReg8(L));
+	printf("AF: 0x%04x, BC: 0x%04x, DE: 0x%04x, HL: 0x%04x, SP: 0x%04x\n", getRegisters().getReg16(AF), getRegisters().getReg16(BC), getRegisters().getReg16(DE), getRegisters().getReg16(HL), getRegisters().getReg16(SP));
+	printf("Last used Memory location: 0x%04x, which contains:\n", getMemory().trace());
 	printf("------------------------------------------------\n");
 }
