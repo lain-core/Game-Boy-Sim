f5044ed2cc819cd81dae35fbd967e0a655db09c4
Hunter King <hking0036@gmail.com>
2020-10-04 16:33:10 -0400

    working stuff

--- asm/first.asm
+++ asm/first.asm
@@ -1,5 +1,10 @@
 INCLUDE "hardware.inc"
-SECTION "Header", ROM0[$100]
+SECTION "Header", ROM0
 EntryPoint:
     di
+    nop 
+    scf
+    ccf 
+    cpl 
+    ei 
     halt
--- gb.cpp
+++ gb.cpp
@@ -12,9 +12,8 @@ void gb::reset(){
 }
 
 // using namespace std;
-int main(){
+int main(int argc, char** argv){
 	gb myGB;
-
 
 	/*uint8_t * miniPix;
 	miniPix = (myGB.getPixel().getRowColor(0x5030));
@@ -24,14 +23,15 @@ int main(){
 	printf("\n");*/
 	
 	//myGB.load("Tetris_World_Rev_1.gb");
+	myGB.load("asm/first.gb");
 	//myGB.getMemory().dumpROM();
-	std::cout << "Entering loop!" << std::endl;
+	//std::cout << "Entering loop!" << std::endl;
 	myGB.setStatus(true);
 	while(myGB.getStatus()){
 		myGB.decode(myGB.getMemory().getWord(myGB.pc++));
 	}
 
-	// //myGB.trace();
+	myGB.trace();
 	// myGB.scf();
 	// std::cout << "cflag: " << myGB.getRegisters().getFlag(FLAG_C) << std::endl;
 	// myGB.ccf();
@@ -51,6 +51,6 @@ void gb::trace(){
 	printf("A: 0x%02x F: 0x%02x\nB: 0x%02x C: 0x%02x\nD: 0x%02x E: 0x%02x\nH: 0x%02x L: 0x%02x\n", getRegisters().getReg8(A), getRegisters().getReg8(F), getRegisters().getReg8(B), getRegisters().getReg8(C) , getRegisters().getReg8(D), getRegisters().getReg8(E), getRegisters().getReg8(H), getRegisters().getReg8(L));
 	printf("AF: 0x%04x, BC: 0x%04x, DE: 0x%04x, HL: 0x%04x, SP: 0x%04x\n", getRegisters().getReg16(AF), getRegisters().getReg16(BC), getRegisters().getReg16(DE), getRegisters().getReg16(HL), getRegisters().getReg16(SP));
 	printf("Memory Error: %d\n", getMemory().isMemError());
-	printf("Last used Memory location: 0x%04x, which contains:\n", getMemory().trace());
+	printf("Last used Memory location: 0x%04x, which contains: %02x\n", getMemory().trace(), getMemory().getByte(getMemory().trace()));
 	printf("------------------------------------------------\n");
 }
