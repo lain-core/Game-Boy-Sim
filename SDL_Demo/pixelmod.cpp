67bca4440e402c5ad480783636e2ee6867c2554a
storyjss <storyjs@appstate.edu>
2020-10-05 13:27:02 -0400

    pushed the wrong file.. oops

--- SDL_Demo/pixelmod.cpp
+++ SDL_Demo/pixelmod.cpp
@@ -9,58 +9,80 @@
 #define COLOR3 0x009bbc0f 
 
 uint32_t miniPix[8];
-uint32_t pixel;
+uint32_t pixels;
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 800;
+
 uint32_t rmask = 0xff000000;
 uint32_t gmask = 0x00ff0000;
 uint32_t bmask = 0x0000ff00;
 uint32_t amask = 0x000000ff;
 
 void init();
-void loadMedia();
+// void loadMedia();
 uint32_t * getRowColor(uint16_t row);
+void smiley();
 
 
 SDL_Window* window = NULL;
+SDL_Renderer* renderma = NULL;
 SDL_Surface* screenSurface = NULL;
-SDL_Surface* stretchSurface = NULL;
+SDL_Surface* stretchedSurface = NULL;
+SDL_Texture* texture = NULL;
+
 
 void init(){
     // initialize sdl
     SDL_Init(SDL_INIT_VIDEO); 
     //create the window
-    window = SDL_CreateWindow("PLEASE WORK", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    screenSurface = SDL_GetWindowSurface(window);
+    window = SDL_CreateWindow("PLEASE WORK", SDL_WINDOWPOS_UNDEFINED, 
+                            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 
+                            SDL_WINDOW_SHOWN);
+    renderma = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    // screenSurface = SDL_GetWindowSurface(window);
+//     texture = SDL_CreateTexture(renderma, SDL_PIXELFORMAT_ABGR8888, 
+//                             SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 
-void loadMedia(){
-    
-}
+// void loadMedia(){
+  
+// }
 
 int main(){
-    uint32_t ** colors = (uint32_t**)malloc(8*sizeof(uint32_t*));
-    for (int i = 0; i < 8; i++){
-        colors[i] = (uint32_t*)malloc(8*sizeof(uint32_t));
+    bool quit = false;
+    SDL_Event e;
+
+    init();
+    smiley();
+    // loadMedia();
+
+    while(!quit){
+        while(SDL_PollEvent(&e)!=0){
+            if (e.type == SDL_QUIT){
+                quit = true;
+            }
+            // else if (e.type == SDL_KEYDOWN ) {
+            //     switch(e.key.keysym.sym){
+            //         case SDLK_UP:
+    
+            //             break();
+            //         default:
+            //             break();
+            //     }
+            // }
+        }
+        SDL_SetRenderDrawColor(renderma, 0x00, 0x00, 0x00, 0x00);
+        SDL_RenderClear(renderma);
+
+        SDL_Rect stretchRect;
+        stretchRect.x = 0;
+        stretchRect.y = 0;
+        stretchRect.w = SCREEN_WIDTH;
+        stretchRect.h = SCREEN_HEIGHT;
+        SDL_BlitScaled(stretchedSurface, NULL, screenSurface, &stretchRect);
+        SDL_UpdateWindowSurface(window);
     }
-
-    // uint32_t *pixels = (unsigned int *)stretchSurface->pixels;
-	// // convert bytearray to rgb values
-	uint16_t smileyTile[8] = {0xFF00, 0xFF00, 0xFF24, 0xFF00, 
-                              0xFF42, 0xFF7E, 0xFF00, 0xFF00};
-
-
-    // uint16_t testTile[8] = {0x0000, 0x0000, 0x0000, 0x0000, 
-    //                         0x0000, 0x0000, 0xFFFF, 0x0000};
-
-    // printf("%x ", *getRowColor(smileyTile[1]));
-
-   for (int i = 0; i < 8; i++){
-       colors[i] = getRowColor(smileyTile[i]);
-       for (int j = 0; j < 8; j++) printf("% 6x ", colors[i][j]);
-       printf("\n");
-   } 
 }
 
 uint32_t * getRowColor(uint16_t row){
@@ -97,4 +119,26 @@ uint32_t * getRowColor(uint16_t row){
             }
         }
         return miniPix;
+}
+
+void smiley(){
+    uint32_t **pixels = (uint32_t **)malloc(8 * sizeof(uint32_t *));
+    for (int i = 0; i < 8; i++)
+    {
+        pixels[i] = (uint32_t *)malloc(8 * sizeof(uint32_t));
+    }
+
+
+    uint16_t smileyTile[8] = {0xFF00, 0xFF00, 0xFF24, 0xFF00,
+                              0xFF42, 0xFF7E, 0xFF00, 0xFF00};
+
+    for (int i = 0; i < 8; i++)
+    {
+        pixels[i] = getRowColor(smileyTile[i]);
+        // for (int j = 0; j < 8; j++)
+        //     printf("% 6x ", pixels[i][j]);
+        // printf("\n");
+    }
+
+    screenSurface = SDL_CreateRGBSurfaceFrom((void*) pixels, SCREEN_WIDTH, SCREEN_HEIGHT, 4, 1, rmask, gmask, bmask, amask); 
 }
