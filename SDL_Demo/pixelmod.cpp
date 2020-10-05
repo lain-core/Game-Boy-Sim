4db7f03cd25c8094b40f1ab4c524ad8cc198a5c2
Joe Story <lefoustory@gmail.com>
2020-10-05 11:53:28 -0400

    Raw Vram to pixels is looking closer to reality now :)

--- SDL_Demo/pixelmod.cpp
+++ SDL_Demo/pixelmod.cpp
@@ -8,7 +8,7 @@
 #define COLOR2 0x008bac0f 
 #define COLOR3 0x009bbc0f 
 
-uint8_t miniPix[8];
+uint32_t miniPix[8];
 uint32_t pixel;
 
 const int SCREEN_WIDTH = 800;
@@ -20,8 +20,8 @@ uint32_t amask = 0x000000ff;
 
 void init();
 void loadMedia();
-uint8_t *getTileColor(uint16_t *row);
-uint32_t intToRGB(uint8_t color);
+uint32_t * getRowColor(uint16_t row);
+
 
 SDL_Window* window = NULL;
 SDL_Surface* screenSurface = NULL;
@@ -40,60 +40,61 @@ void loadMedia(){
 }
 
 int main(){
-    uint32_t colors[8] = {0};
+    uint32_t ** colors = (uint32_t**)malloc(8*sizeof(uint32_t*));
+    for (int i = 0; i < 8; i++){
+        colors[i] = (uint32_t*)malloc(8*sizeof(uint32_t));
+    }
+
     // uint32_t *pixels = (unsigned int *)stretchSurface->pixels;
-	// convert bytearray to rgb values
-	uint16_t smileyTile[8] = {0xFF00, 0xFF00,
-                            0xFF24, 0xFF00, 
-                            0xFF42, 0xFF7E, 
-                            0xFF00, 0xFF00};
+	// // convert bytearray to rgb values
+	uint16_t smileyTile[8] = {0xFF00, 0xFF00, 0xFF24, 0xFF00, 
+                              0xFF42, 0xFF7E, 0xFF00, 0xFF00};
 
 
     // uint16_t testTile[8] = {0x0000, 0x0000, 0x0000, 0x0000, 
-    //                         0x0000, 0x0000, 0x0000, 0x0000};
+    //                         0x0000, 0x0000, 0xFFFF, 0x0000};
 
-    colors = getTileColor(smileyTile[1]);
+    // printf("%x ", *getRowColor(smileyTile[1]));
 
    for (int i = 0; i < 8; i++){
-       printf("%x ", colors[i]);
-   } printf("\n");
+       colors[i] = getRowColor(smileyTile[i]);
+       for (int j = 0; j < 8; j++) printf("% 6x ", colors[i][j]);
+       printf("\n");
+   } 
 }
 
-uint32_t intToRGB(uint8_t color){
-    uint8_t temp;
-	switch(color){
-		case 0:
-			temp = COLOR0;
-			break;
-
-		case 1:
-			temp = COLOR1;
-			break;
-			
-		case 2:
-			temp = COLOR2;
-			break;
-			
-		case 3:
-			temp = COLOR3;
-			break;
-			
-		default:
-			break;
-	}
-    return temp;
-}
-
-uint32_t * getTileColor(uint16_t row){
-        uint32_t colorArray[8] = {0};
+uint32_t * getRowColor(uint16_t row){
+    for (int i = 0; i < 8; i++){
+        miniPix[i] = -1;
+    }
         uint16_t byte1 = (row & 0xFF00) >> 8; // grab high order byte out of the 2
         uint16_t byte2 = (row & 0x00FF);      // grab low order byte
         uint16_t mask = 0x01;
 
         for (int i = 0; i < 8; i++){
-            if (byte1 & (mask << i)) miniPix[7-i] += 1; // if byte 1's bit is on
-            if (byte2 & (mask << i)) miniPix[7-i] += 2; // if byte 2's bit is on
-            colorArray[i] = intToRGB(miniPix[i]); 
+            if (byte1 & (mask << i)) miniPix[7-i] += 2; // if byte 1's bit is on
+            if (byte2 & (mask << i)) miniPix[7-i] += 3; // if byte 2's bit is on
+            switch (miniPix[7-i]){
+            case 0:
+                miniPix[7-i] = COLOR0;
+                break;
+
+            case 1:
+                miniPix[7-i] = COLOR1;
+                break;
+
+            case 2:
+                miniPix[7-i] = COLOR2;
+                break;
+
+            case 3:
+                miniPix[7-i] = COLOR3;
+                break;
+
+            default:
+                miniPix[7-i] = 0x00000000;
+                break;
+            }
         }
-    return colorArray;
+        return miniPix;
 }
