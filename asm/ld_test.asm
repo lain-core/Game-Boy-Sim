114af2b16ef52e48d3a7f980092370c3e6dd659c
storyjss <lefoustory@gmail.com>
2020-11-08 20:04:36 -0500

    load testing

--- asm/ld_test.asm
+++ asm/ld_test.asm
@@ -1,14 +1,19 @@
 INCLUDE "hardware.inc"
 SECTION "Header", ROM0
 EntryPoint:
+    ld b, $04
     ;LD r8,r8
+    ld a, b
     ;LD r8,n8
     ld a, $ff ;A should now contain 0xFF.
     ;LD r16,n16
-    ld bc, $ffff ; BC should now contain 0xFFFF.
+    ld hl, $a000 ; HL should now contain 0xA000.
     ;LD [HL],r8
+    ld [HL], a ; 0xA000 in memory should now contain 0xFF.
     ;LD [HL],n8
+    ld [HL], $0A ; 0xA000 in memory should now contain 0x0A.
     ;LD r8,[HL]
+    ld b, [hl] ;B should now contain 0x0A.
     ;LD [r16],A
     ;LD [n16],A
     ;LDH [n16],A
