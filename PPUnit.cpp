5244c87758435cf20fbb2f2eeb51628a1cd1c9e7
storyjss <lefoustory@gmail.com>
2020-11-20 15:42:22 -0500

    Beginning to build the PPU's process

--- PPUnit.cpp
+++ PPUnit.cpp
@@ -31,6 +31,17 @@ uint8_t * PPUnit::getRowColor(uint16_t row){
     return miniPix;
 }
 
+/*
+ * scan_line()
+ * render one scan line.
+ */
+void PPUnit::scan_line(){
+    uint8_t control = memory->getByte(LCD_CONTROL);
+    uint8_t test = 1;
+    if(control & (test << BG_DISPLAY)) render_tiles();
+    //if(control & (test << OBJ_DISPLAY)) render_sprites;
+}
+
 void PPUnit::render_tiles(){
 // where to draw the visual area and the window
    uint8_t scrollY = memory->getByte(SCROLL_Y);
@@ -38,4 +49,6 @@ void PPUnit::render_tiles(){
    uint8_t windowY = memory->getByte(WINDOW_Y);
    uint8_t windowX = memory->getByte(WINDOW_X) - 7;
    printf("ScrollY: %02x\n", memory->getByte(SCROLL_Y));
+
+   
 }
